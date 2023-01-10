1d4f1e2a9201fcb0667dcab810d142d61fc14b11
Kyle Cunnison <kylecunnison@gmail.com>
2023-01-11 00:05:18 +0100

    added edit and delete functionality

--- src/main/java/com/aylin/notes/config/CorsGlobalConfig.java
+++ src/main/java/com/aylin/notes/config/CorsGlobalConfig.java
@@ -1,8 +1,11 @@
 package com.aylin.notes.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+@Configuration
 public class CorsGlobalConfig implements WebFluxConfigurer {
 
     @Override
--- src/main/java/com/aylin/notes/controller/NoteController.java
+++ src/main/java/com/aylin/notes/controller/NoteController.java
@@ -2,17 +2,17 @@ package com.aylin.notes.controller;
 
 import com.aylin.notes.model.Note;
 import com.aylin.notes.repository.NoteRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 
 @RestController
 @RequestMapping("/notes")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","https://note-front-gamma.vercel.app"})
 public class NoteController {
     private final NoteRepository noteRepository;
 
@@ -34,5 +34,26 @@ public class NoteController {
     private Mono<Note> insertNote(@RequestBody Note note) {
         note.created = LocalDateTime.now(ZoneOffset.UTC);
         return noteRepository.save(note);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    private Mono<Void> deleteNote(@PathVariable String id) {
+        return noteRepository.findById(id)
+                .switchIfEmpty(Mono.error(new Exception("NOTE_NOT_FOUND")))
+                .flatMap(note -> noteRepository.deleteById(note.getId()));
+    }
+
+    @PutMapping("/edit")
+    @ResponseStatus(HttpStatus.OK)
+    private Mono<Note> editNote(@RequestBody Note note) {
+        return noteRepository.findById(note.getId())
+                .switchIfEmpty(Mono.error(new Exception("NOTE_NOT_FOUND")))
+                .map(fetchedNote -> {
+                    fetchedNote.setColour(note.colour);
+                    fetchedNote.setContent(note.content);
+                    fetchedNote.setFontColour(note.fontColour);
+                            return fetchedNote;
+                }).flatMap(noteRepository::save);
     }
 }
